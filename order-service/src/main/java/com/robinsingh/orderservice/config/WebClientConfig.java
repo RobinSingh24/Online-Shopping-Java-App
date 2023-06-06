a51188985e1d871a81d9032d4ea38c05f0ca400c
Robin SIngh <robin.122497@gmail.com>
2023-06-06 13:34:50 +0530

    Added WebFlux dependency

---
+++ order-service/src/main/java/com/robinsingh/orderservice/config/WebClientConfig.java
@@ -0,0 +1,9 @@
+package com.robinsingh.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class WebClientConfig {
+
+
+}
