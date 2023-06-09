8d3e33184935d91e81c9b7a0647292341dab3d39
Robin SIngh <robin.122497@gmail.com>
2023-06-10 00:05:01 +0530

    Added Neftlix Eureka config

--- product-service/src/main/java/com/robinsingh/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/robinsingh/productservice/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.robinsingh.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
